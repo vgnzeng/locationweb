8a7e8a19ff7bf9951260d102dfb86859b1115e2d
Guannian Zeng <vgnzeng@gmail.com>
2018-08-27 18:04:26 -0700

    added Service Layer

---
+++ src/main/java/com/zeng/location/service/LocationService.java
@@ -0,0 +1,18 @@
+package com.zeng.location.service;
+
+import java.util.List;
+
+import com.zeng.location.entities.Location;
+
+public interface LocationService {
+
+	Location saveLocation(Location location);
+
+	Location updateLocation(Location location);
+
+	void deleteLocation(Location location);
+
+	Location getLocationById(int id);
+
+	List<Location> getAllLocations();
+}
---
+++ src/main/java/com/zeng/location/service/LocationServiceImpl.java
@@ -0,0 +1,50 @@
+package com.zeng.location.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.zeng.location.entities.Location;
+import com.zeng.location.repos.LocationRepository;
+
+@Service
+public class LocationServiceImpl implements LocationService {
+
+	@Autowired
+	private LocationRepository repository;
+
+	@Override
+	public Location saveLocation(Location location) {
+		return repository.save(location);
+	}
+
+	@Override
+	public Location updateLocation(Location location) {
+		return repository.save(location);
+	}
+
+	@Override
+	public void deleteLocation(Location location) {
+		repository.delete(location);
+	}
+
+	@Override
+	public Location getLocationById(int id) {
+		return repository.findOne(id);
+	}
+
+	@Override
+	public List<Location> getAllLocations() {
+		return repository.findAll();
+	}
+
+	public LocationRepository getRepository() {
+		return repository;
+	}
+
+	public void setRepository(LocationRepository repository) {
+		this.repository = repository;
+	}
+
+}
