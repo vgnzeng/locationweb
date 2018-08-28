806443bfee501832d2e80e2dd5183dd1c9abe27a
Guannian Zeng <vgnzeng@gmail.com>
2018-08-27 18:48:05 -0700

    added method for saving Location instance and using ModelMap to send response back to Broswer

--- src/main/java/com/zeng/location/controllers/LocationController.java
+++ src/main/java/com/zeng/location/controllers/LocationController.java
@@ -1,14 +1,32 @@
 package com.zeng.location.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.zeng.location.entities.Location;
+import com.zeng.location.service.LocationService;
 
 @Controller
 public class LocationController {
 
+	@Autowired
+	LocationService service;
+	
 	@RequestMapping("/showCreate")
 	public String showCreate() {
 		return "createLocation";
 	}
+	
+	@RequestMapping("/saveLoc")
+	public String saveLocation(@ModelAttribute("location") Location location, ModelMap modelMap) {
+		Location locationSaved = service.saveLocation(location);
+		String msg = "Location saved with id: " + locationSaved.getId();
+		modelMap.addAttribute("msg", msg);
+		return "createLocation";
+	}
+
 
 }
