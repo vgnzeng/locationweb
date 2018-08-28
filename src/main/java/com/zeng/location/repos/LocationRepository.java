3f1202253f2352693e8b09f095b3025747358557
Guannian Zeng <vgnzeng@gmail.com>
2018-08-28 09:00:55 -0700

    upaded repository with method that group by column

--- src/main/java/com/zeng/location/repos/LocationRepository.java
+++ src/main/java/com/zeng/location/repos/LocationRepository.java
@@ -9,4 +9,7 @@ import com.zeng.location.entities.Location;
 
 public interface LocationRepository extends JpaRepository<Location, Integer> {
 
+	@Query("select type,count(type) from Location group by type")
+	public List<Object[]> findTypeAndTypeCount();
+	
 }
